a63e1ef2306dea160fb10da06863bb6cf987951a
Kyle Michaels <kyle.michaels@gmail.com>
2024-02-17 23:42:26 -0800

    Added Firestore

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
+// Import Screens
 import Start from './components/Start';
 import Chat from './components/Chat';
 
+// Import React Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// Create the navigator
 const Stack = createNativeStackNavigator();
 
+// Import Firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Import and ignore warning log
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['@firebase/auth: Auth (10.3.1):']);
+
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCUlYwB9aWUXh7zB1VZ9Fs-27mNfhFxjls",
+    authDomain: "chat-app-6b059.firebaseapp.com",
+    projectId: "chat-app-6b059",
+    storageBucket: "chat-app-6b059.appspot.com",
+    messagingSenderId: "331405396880",
+    appId: "1:331405396880:web:4b859973f2c704910e8115"
+  };
+  // Initialize firebase and get a reference to database
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -18,8 +41,9 @@ const App = () => {
         />
         <Stack.Screen
           name='Chat'
-          component={Chat}
-        />
+        >
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
